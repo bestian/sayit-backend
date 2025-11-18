2d139366397511537e91a2d533ccb6511e9dcbf4
Bestian Tang <109048+bestian@users.noreply.github.com>
2025-11-19 07:13:33 +0800

    對講者表建立第一筆填充資料，修改並確立格式, work on #2
    只是確立格式，未來需要修改為完整

--- sql/init-speakers.sql
+++ sql/init-speakers.sql
@@ -4,6 +4,23 @@ CREATE TABLE IF NOT EXISTS speakers (
     id INTEGER PRIMARY KEY NOT NULL,
     name TEXT NOT NULL,
     photoURL TEXT,
-    speeches INTEGER DEFAULT 0,
-    longest_speech INTEGER DEFAULT 0
+	appearances_count INTEGER DEFAULT 0,
+    speeches_count INTEGER DEFAULT 0,
+    speeches TEXT, -- JSON array of speeches
+    longest_speech TEXT -- JSON object of longest speech
 );
+
+
+
+insert into speakers (name, photoURL, appearances_count, speeches_count, speeches, longest_speech) -- 目前是第一筆填充資料，只是確立格式，未來需要修改為完整
+	values ('唐鳳',
+	    'https://sayit.archive.tw/media/speakers/default/pic_AudreyTang-small.jpg.96x96_q85_crop-smart_face_upscale.jpg',
+		1036,
+		95581,
+		'[
+			{"summary": "謝謝大家。","speech_name": "2025-11-10-柏林自由會議ai-的角色", "section_id": 628242},
+			{"summary": "聽說「奇點即將接近」—— 多元宇宙，已經來臨。","speech_name": "2025-11-10-柏林自由會議ai-的角色", "section_id": 628241}
+
+		]',
+		'{"summary": "...","speech_name": "2025-03-03-日經新聞採訪", "section_id": 621538}');
+

3a2d11f7775c6df062ad7b1a42e9f376c4d25843
Bestian Tang <109048+bestian@users.noreply.github.com>
2025-11-19 07:14:16 +0800

    對演講索引建立第一筆資料，確立格式，work on #2

--- sql/init-speech_index.sql
+++ sql/init-speech_index.sql
@@ -1,7 +1,14 @@
 DROP TABLE IF EXISTS speech_index;
 
 CREATE TABLE IF NOT EXISTS speech_index (
-    id INTEGER PRIMARY KEY NOT NULL,
-    filename TEXT NOT NULL,
-    speakers TEXT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    filename TEXT NOT NULL UNIQUE
+    -- speakers TEXT
 );
+
+insert into speech_index (filename) values ('2025-11-10-柏林自由會議：AI 的角色');
+insert into speech_index (filename) values ('2025-11-10-Berlin Freedom Conference: The Role of AI');
+insert into speech_index (filename) values ('2025-11-08-解學習：監管');
+insert into speech_index (filename) values ('2025-11-08-Unlearning Regulation');
+insert into speech_index (filename) values ('2025-11-06-開放 AI 安全，巨頭迎來轉折點');
+

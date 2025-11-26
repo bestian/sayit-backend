fe2506c74d52ca52623097517071db07ad41edf2
Bestian Tang <109048+bestian@users.noreply.github.com>
2025-11-26 13:37:40 +0800

    重設sql/init-speech_index.sql, work on #17

--- sql/init-speech_index.sql
+++ sql/init-speech_index.sql
@@ -2,13 +2,11 @@ DROP TABLE IF EXISTS speech_index;
 
 CREATE TABLE IF NOT EXISTS speech_index (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    filename TEXT NOT NULL UNIQUE
+    filename TEXT NOT NULL UNIQUE,
+    display_name TEXT NOT NULL
     -- speakers TEXT
 );
 
-insert into speech_index (filename) values ('2025-11-10-柏林自由會議：AI 的角色');
-insert into speech_index (filename) values ('2025-11-10-Berlin Freedom Conference: The Role of AI');
-insert into speech_index (filename) values ('2025-11-08-解學習：監管');
-insert into speech_index (filename) values ('2025-11-08-Unlearning Regulation');
-insert into speech_index (filename) values ('2025-11-06-開放 AI 安全，巨頭迎來轉折點');
+insert into speech_index (filename, display_name) values ('2025-11-10-柏林自由會議ai-的角色', '2025-11-10 柏林自由會議：AI 的角色');
+insert into speech_index (filename, display_name) values ('2025-11-10-berlin-freedom-conference-the-role-of-a', '2025-11-10 Berlin Freedom Conference: The Role of AI');
 

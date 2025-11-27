e60fdb64882af8df34d71b1c4ed2ab02c3fc3753
Bestian Tang <109048+bestian@users.noreply.github.com>
2025-11-28 07:49:18 +0800

    段落索引建立加入講者索引, work on #21

--- sql/add-speech_content-filename-index.sql
+++
@@ -1,6 +0,0 @@
--- 為 speech_content 表的 filename 欄位建立索引
--- 此索引可優化 /api/speech/{filename} 路由的查詢效能
--- 當資料量較大時，建議執行此 SQL 來建立索引
-
-CREATE INDEX IF NOT EXISTS idx_speech_content_filename ON speech_content(filename);
-

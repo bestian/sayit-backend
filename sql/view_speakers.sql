e2dcc57eb8402630d9ec68a029cafb17b2b219f1
Bestian Tang <109048+bestian@users.noreply.github.com>
2026-01-07 05:33:26 +0800

    Update view_speakers.sql

--- sql/view_speakers.sql
+++ sql/view_speakers.sql
@@ -1,65 +1,49 @@
 DROP VIEW IF EXISTS speakers_view;
 
 CREATE VIEW speakers_view AS
+WITH longest_section AS (
+    SELECT
+        sc.section_speaker,
+        sc.section_id,
+        sc.section_content,
+        sc.filename,
+        sc.nest_filename,
+        sc.nest_display_name,
+        si.display_name,
+        ROW_NUMBER() OVER (
+            PARTITION BY sc.section_speaker
+            ORDER BY LENGTH(sc.section_content) DESC, sc.section_id ASC
+        ) AS rn
+    FROM speech_content sc
+    LEFT JOIN speech_index si ON sc.filename = si.filename
+    WHERE sc.section_content IS NOT NULL
+      AND sc.section_content != ''
+      AND sc.section_speaker IS NOT NULL
+      AND sc.section_speaker != ''
+)
 SELECT
     s.id,
     s.route_pathname,
     s.name,
     s.photoURL,
-    -- 計算 appearances_count：僅針對目前 speaker
     COALESCE((
         SELECT COUNT(DISTINCT ss.speech_filename)
         FROM speech_speakers ss
         WHERE ss.speaker_route_pathname = s.route_pathname
     ), 0) AS appearances_count,
-    -- 計算 sections_count：僅針對目前 speaker
     COALESCE((
         SELECT COUNT(DISTINCT sc.section_id)
         FROM speech_content sc
         WHERE sc.section_speaker = s.route_pathname
     ), 0) AS sections_count,
-    -- longest_section 相關欄位：從 speech_content 找最長的 section_content
-    longest_section.section_id AS longest_section_id,
-    longest_section.section_content AS longest_section_content,
-    longest_section.filename AS longest_section_filename,
-    longest_section.nest_filename AS longest_section_nest_filename,
-    longest_section.nest_display_name AS longest_section_nest_display_name,
-    longest_section.display_name AS longest_section_displayname
-FROM
-    speakers s
-    -- 子查詢：找每個 speaker 最長的 section
-    LEFT JOIN (
-        SELECT
-            sc.section_speaker,
-            sc.section_id,
-            sc.section_content,
-            sc.filename,
-            sc.nest_filename,
-            sc.nest_display_name,
-            si.display_name
-        FROM
-            speech_content sc
-            LEFT JOIN speech_index si ON sc.filename = si.filename
-        WHERE
-            sc.section_content IS NOT NULL
-            AND sc.section_content != ''
-            AND sc.section_speaker IS NOT NULL
-            AND sc.section_speaker != ''
-            AND sc.section_id = (
-                SELECT
-                    sc2.section_id
-                FROM
-                    speech_content sc2
-                WHERE
-                    sc2.section_speaker = sc.section_speaker
-                    AND sc2.section_speaker IS NOT NULL
-                    AND sc2.section_speaker != ''
-                    AND sc2.section_content IS NOT NULL
-                    AND sc2.section_content != ''
-                ORDER BY
-                    LENGTH(sc2.section_content) DESC,
-                    sc2.section_id ASC
-                LIMIT 1
-            )
-    ) longest_section ON s.route_pathname = longest_section.section_speaker;
+    ls.section_id AS longest_section_id,
+    ls.section_content AS longest_section_content,
+    ls.filename AS longest_section_filename,
+    ls.nest_filename AS longest_section_nest_filename,
+    ls.nest_display_name AS longest_section_nest_display_name,
+    ls.display_name AS longest_section_displayname
+FROM speakers s
+LEFT JOIN longest_section ls
+    ON s.route_pathname = ls.section_speaker
+    AND ls.rn = 1;
 
